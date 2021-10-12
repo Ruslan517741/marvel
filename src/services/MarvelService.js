2938f2749e17d3861165a698c43ee1a89918b98a
Rus <mamail>
2021-10-12 15:57:06 +0300

    +MarvelService.js

---
+++ src/services/MarvelService.js
@@ -0,0 +1,28 @@
+
+
+class MarvelService {
+    _apiBase = 'https://gateway.marvel.com:443/v1/public/';
+    _apiKey = 'apikey=ad796d198ab7f64772092abd9c4347d0';
+
+    getResource = async (url) => {
+        const res = await fetch(url);
+    
+        if (!res.ok) {
+            throw new Error(`Could not fetch ${url}, status: ${res.status}`);
+        }
+    
+        return await res.json();
+    };
+
+    getAllCharacters = () => {
+        return this.getResource(`${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`);
+        
+    }
+
+    getCharacter = (id) => {
+        return this.getResource(`${this._apiBase}characters/${id}?${this._apiKey}`);
+        
+    }
+}
+
+export default MarvelService;
